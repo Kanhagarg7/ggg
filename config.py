ebc3f5860bd96091defd5d0677ee8534edf8a099
Kanhagarg7 <162786866+Kanhagarg7@users.noreply.github.com>
2024-11-13 10:40:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = 1747534
 API_HASH = "5a2684512006853f2e48aca9652d83ea"
 
-BOT_TOKEN = "7080419510:AAGga5Kfmhde42IVbhBejdJ1YLoIqbst6LM"
+BOT_TOKEN = "6333603085:AAGZi1xcankYpaqIhdlaLAhv606PX2hUntk"
 OWNER_ID = 6775943525
 
 MONGO_DB_URI = "mongodb+srv://kanhagarg930:<db_password>@cluster0.peanlql.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
